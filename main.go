33a3ae2404237a4b96e48ac6b356ee2b62c2cd15
jmichalak9 <michalakjakub1999@gmail.com>
2020-04-04 21:59:32 +0200

    Refactor: handlers now take client instead of connection

--- main.go
+++ main.go
@@ -7,44 +7,85 @@ import (
 	"strings"
 )
 
-type commandHandler func(string) error
+type client struct {
+	conn     net.Conn
+	username string
+	datatype string
+}
+type commandHandler func(client, string) error
 
 var handlers = map[string]commandHandler{
 	"USER": handleUSER,
+	"PASS": handlePASS,
+	"SYST": handleSYST,
+	"PWD":  handlePWD,
+	"TYPE": handleTYPE,
 }
 
-func handleUSER(argv string) error {
-	fmt.Printf("USER handler called with %s\n", argv)
+func handleUSER(c client, argv string) error {
+	c.username = argv
+	if argv == "anonymous" {
+		fmt.Println("Logging user anonymous")
+		c.conn.Write([]byte("331 User name ok, need password\r\n"))
+	} else {
+		fmt.Println("Logging unknown user")
+		c.conn.Write([]byte("331 User name ok, need password\r\n"))
+	}
 	return nil
 }
 
-func handleConnection(c net.Conn) {
+func handlePASS(c client, argv string) error {
+	c.conn.Write([]byte("230 User logged in, proceed\r\n"))
+	return nil
+}
+
+func handleSYST(c client, argv string) error {
+	c.conn.Write([]byte("215 UNIX Type: L8\r\n"))
+	return nil
+}
+
+func handlePWD(c client, argv string) error {
+	//pwd, _ := os.Getwd()
+	c.conn.Write([]byte("217 " + " \r\n"))
+	return nil
+}
+
+func handleTYPE(c client, argv string) error {
+	c.datatype = argv
+	c.conn.Write([]byte("200 Command okay.\r\n"))
+	return nil
+}
+
+func handleConnection(conn net.Conn) {
 	fmt.Println("new connection")
-	c.Write([]byte("220 FTP server\r\n"))
+	c := client{
+		conn: conn,
+	}
+	c.conn.Write([]byte("220 FTP server\r\n"))
 	for {
-		netData, err := bufio.NewReader(c).ReadString('\n')
+		netData, err := bufio.NewReader(c.conn).ReadString('\n')
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
+		fmt.Print(string(netData))
 
 		line := strings.SplitN(strings.TrimSpace(string(netData)), " ", 2)
-		if len(line) < 2 {
-			break
+		// if the command is without arguments
+		if len(line) == 1 {
+			line = append(line, "")
 		}
 		cmd, argv := line[0], line[1]
 		if handler, ok := handlers[cmd]; ok {
-			handler(argv)
+			handler(c, argv)
 		}
 		// Only temporary. TODO: handle connection end.
 		if cmd == "STOP" {
 			break
 		}
 
-		fmt.Println(line)
-		c.Write([]byte("sent " + string(netData)))
 	}
-	c.Close()
+	c.conn.Close()
 }
 
 func main() {

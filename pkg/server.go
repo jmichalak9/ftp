b98f5a51b4b4557c0f05da78aa15402977204b5b
jmichalak9 <michalakjakub1999@gmail.com>
2020-05-05 14:37:51 +0200

    Refactor server

--- pkg/server.go
+++ pkg/server.go
@@ -12,81 +12,164 @@ import (
 	"time"
 )
 
+// Server represents an FTP server.
+type Server struct {
+	Addr string
+}
+
+// Client represents an FTP client. It is used by a server for communication.
 type client struct {
 	conn     net.Conn
 	username string
 	pwd      string
 	datatype string
-	listener net.Listener
-}
-
-// Server represents an FTP server
-type Server struct {
-	Addr string
+	dataSock net.Listener
+	toClose  bool
 }
 
 type commandHandler func(*client, string) error
 
 var handlers = map[string]commandHandler{
-	"USER": handleUSER,
-	"PASS": handlePASS,
-	"SYST": handleSYST,
-	"PWD":  handlePWD,
 	"CWD":  handleCWD,
-	"TYPE": handleTYPE,
-	"SIZE": handleSIZE,
-	"PASV": handlePASV,
+	"FEAT": handleFEAT,
 	"LIST": handleLIST,
+	"MDTM": handleMDTM,
+	"PASS": handlePASS,
+	"PASV": handlePASV,
+	"PWD":  handlePWD,
 	"QUIT": handleQUIT,
 	"RETR": handleRETR,
-	"FEAT": handleFEAT,
-	"MDTM": handleMDTM,
+	"SIZE": handleSIZE,
+	"SYST": handleSYST,
+	"TYPE": handleTYPE,
+	"USER": handleUSER,
 }
 
-func handleUSER(c *client, argv string) error {
-	c.username = argv
-	if argv == "anonymous" {
-		fmt.Println("Logging user anonymous")
-		c.conn.Write([]byte("331 User name ok, need password\r\n"))
-	} else {
-		fmt.Println("Logging unknown user")
-		c.conn.Write([]byte("331 User name ok, need password\r\n"))
-	}
-	return nil
-}
-
-func handlePASS(c *client, argv string) error {
-	c.conn.Write([]byte("230 User logged in, proceed\r\n"))
-	return nil
-}
-
-func handleSYST(c *client, argv string) error {
-	c.conn.Write([]byte("215 UNIX Type: L8\r\n"))
-	return nil
-}
-
-func handlePWD(c *client, argv string) error {
-	c.conn.Write([]byte("217 /\r\n"))
-	return nil
-}
+var eol = "\r\n"
 
 func handleCWD(c *client, argv string) error {
 	file, err := getItemFromPath(argv)
 	if err != nil || reflect.TypeOf(file).String() != "map[string]interface {}" {
-		c.conn.Write([]byte("550 file not found\r\n"))
-		return nil
+		c.conn.Write([]byte(ReplyFileUnavailable + eol))
+		return err
 	}
 	c.pwd = argv
-	c.conn.Write([]byte("250 OK\r\n"))
+	_, err = c.conn.Write([]byte(ReplyFileActionOK + eol))
+	return err
+}
+
+func handleFEAT(c *client, argv string) error {
+	_, err := c.conn.Write([]byte("211-Features:\r\n" +
+		"211 End\r\n"))
+	return err
+}
+
+func handleLIST(c *client, argv string) error {
+	conn, err := c.dataSock.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	c.conn.Write([]byte(ReplyFileStatusOK + eol))
+	dir, err := getItemFromPath(c.pwd)
+	if reflect.TypeOf(dir).String() != "map[string]interface {}" || err != nil {
+		// TODO: write error to c.conn
+		return nil
+	}
+	reply := ""
+	for k := range dir.(map[string]interface{}) {
+		// TODO: send proper file properties
+		reply += ("-rwxr-xr-x  10 kuba wheel 776 Nov 24 15:50 " + k + "\r\n")
+	}
+	conn.Write([]byte(reply))
+	conn.Close()
+	c.conn.Write([]byte(ReplyClosingDataConn + eol))
 	return nil
+}
+
+func handleMDTM(c *client, argv string) error {
+	file, err := getItemFromPath(argv)
+	if err != nil || reflect.TypeOf(file).String() != "string" {
+		c.conn.Write([]byte(ReplyFileUnavailable + eol))
+		return err
+	}
+	_, err = c.conn.Write([]byte("213 207001010000\r\n"))
+	return err
+}
+
+func handlePASS(c *client, argv string) error {
+	_, err := c.conn.Write([]byte(ReplyUserLoggedIn + eol))
+	return err
+}
+
+func handlePASV(c *client, argv string) error {
+	ip := []int{127, 0, 0, 1}
+	port := c.dataSock.Addr().(*net.TCPAddr).Port
+	_, err := c.conn.Write([]byte(fmt.Sprintf(ReplyEnteringPasv,
+		ip[0], ip[1], ip[2], ip[3], port/256, port%256) + eol))
+	return err
+}
+
+func handlePWD(c *client, argv string) error {
+	_, err := c.conn.Write([]byte(fmt.Sprintf(ReplyPathNameOK, c.pwd) + eol))
+	return err
+}
+
+func handleQUIT(c *client, argv string) error {
+	c.toClose = true
+	_, err := c.conn.Write([]byte(ReplyClosingConn + eol))
+	return err
+}
+
+func handleRETR(c *client, argv string) error {
+	conn, err := c.dataSock.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	c.conn.Write([]byte(ReplyFileStatusOK + eol))
+	file, err := getItemFromPath(argv)
+	fmt.Printf("%s %v", argv, file)
+	if reflect.TypeOf(file).String() != "string" || err != nil {
+		// TODO: write error to c.conn
+		return nil
+	}
+	conn.Write([]byte(file.(string)))
+	conn.Close()
+	c.conn.Write([]byte(ReplyClosingDataConn + eol))
+	return nil
+}
+
+func handleSIZE(c *client, argv string) error {
+	file, err := getItemFromPath(argv)
+	if err != nil || reflect.TypeOf(file).String() != "string" {
+		c.conn.Write([]byte(ReplyFileUnavailable + eol))
+		return nil
+	}
+	_, err = c.conn.Write([]byte(
+		fmt.Sprintf(ReplyFileStatus, strconv.Itoa(len(file.(string)))) + eol))
+	return err
+}
+
+func handleSYST(c *client, argv string) error {
+	_, err := c.conn.Write([]byte(ReplySystemType + eol))
+	return err
+}
+
+func handleUSER(c *client, argv string) error {
+	c.username = argv
+	_, err := c.conn.Write([]byte(ReplyUserNameOK + eol))
+	return err
 }
 
 func handleTYPE(c *client, argv string) error {
 	c.datatype = argv
-	c.conn.Write([]byte("200 Command okay.\r\n"))
-	return nil
+	_, err := c.conn.Write([]byte(ReplyCmdOK + eol))
+	return err
 }
 
+// pathToSlice slices a path string to a slice of strings.
+// e.g. "/home/example/aaa" -> ["home", "example", "aaa"]
 func pathToSlice(path string) []string {
 	normalised := strings.TrimPrefix(path, "/")
 	return strings.Split(normalised, "/")
@@ -119,101 +202,23 @@ func getItemFromPath(path string) (interface{}, error) {
 	return searchDir, nil
 }
 
-func handleSIZE(c *client, argv string) error {
-	file, err := getItemFromPath(argv)
-	if err != nil || reflect.TypeOf(file).String() != "string" {
-		c.conn.Write([]byte("550 Cannot read file size.\r\n"))
-		return nil
-	}
-	c.conn.Write([]byte("213 " + strconv.Itoa(len(file.(string))) + "\r\n"))
-	return nil
-}
-
-func handleQUIT(c *client, argv string) error {
-	c.conn.Write([]byte("221 Service closing control connection.\r\n"))
-	return c.conn.Close()
-}
-
-func handleFEAT(c *client, argv string) error {
-	c.conn.Write([]byte("211-Features:\r\n" +
-		"211 End\r\n"))
-	return nil
-}
-
-func handleMDTM(c *client, argv string) error {
-	file, err := getItemFromPath(argv)
-	if err != nil || reflect.TypeOf(file).String() != "string" {
-		c.conn.Write([]byte("550 Cannot read file.\r\n"))
-		return nil
-	}
-	c.conn.Write([]byte("213 207001010000\r\n"))
-	return nil
-}
-func handlePASV(c *client, argv string) error {
-	ip := []int{127, 0, 0, 1}
-	port := c.listener.Addr().(*net.TCPAddr).Port
-	c.conn.Write([]byte(fmt.Sprintf("227 Entering Passive Mode (%d,%d,%d,%d,%d,%d)\r\n",
-		ip[0], ip[1], ip[2], ip[3], port/256, port%256)))
-	return nil
-}
-
-func handleLIST(c *client, argv string) error {
-	conn, err := c.listener.Accept()
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	c.conn.Write([]byte("150 File status okay; about to open data connection.\r\n"))
-	dir, err := getItemFromPath(c.pwd)
-	if reflect.TypeOf(dir).String() != "map[string]interface {}" || err != nil {
-		// TODO: write error to c.conn
-		return nil
-	}
-	reply := ""
-	for k := range dir.(map[string]interface{}) {
-		// TODO: send proper file properties
-		reply += ("-rwxr-xr-x  10 kuba wheel 776 Nov 24 15:50 " + k + "\r\n")
-	}
-	conn.Write([]byte(reply))
-	conn.Close()
-	c.conn.Write([]byte("226 Closing data connection.\r\n"))
-	return nil
-}
-
-func handleRETR(c *client, argv string) error {
-	conn, err := c.listener.Accept()
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	c.conn.Write([]byte("150 File status okay; about to open data connection.\r\n"))
-	file, err := getItemFromPath(argv)
-	fmt.Printf("%s %v", argv, file)
-	if reflect.TypeOf(file).String() != "string" || err != nil {
-		// TODO: write error to c.conn
-		return nil
-	}
-	conn.Write([]byte(file.(string)))
-	conn.Close()
-	c.conn.Write([]byte("226 Closing data connection.\r\n"))
-	return nil
-}
-
+// handleConnection handles a connection with an individual client.
 func handleConnection(conn net.Conn) {
-	// create additional port to transfer files
-	listener, err := net.Listen("tcp4", ":0")
+	// Open additional port for data connection.
+	dataSock, err := net.Listen("tcp4", ":0")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer listener.Close()
+	defer dataSock.Close()
 	c := client{
 		conn:     conn,
-		listener: listener,
+		dataSock: dataSock,
+		pwd:      "/",
 	}
 	c.conn.SetDeadline(time.Now().Add(time.Minute))
 	defer c.conn.Close()
-	c.conn.Write([]byte("220 FTP server\r\n"))
+	c.conn.Write([]byte(ReplyServiceReady + eol))
 
 	for {
 		netData, err := bufio.NewReader(c.conn).ReadString('\n')
@@ -224,13 +229,20 @@ func handleConnection(conn net.Conn) {
 		fmt.Print(string(netData))
 
 		line := strings.SplitN(strings.TrimSpace(string(netData)), " ", 2)
-		// if the command is without arguments
+		// If the command is without arguments
 		if len(line) == 1 {
 			line = append(line, "")
 		}
 		cmd, argv := line[0], line[1]
 		if handler, ok := handlers[cmd]; ok {
 			handler(&c, argv)
+		} else {
+			c.conn.Write([]byte(ReplyNotImplemented + eol))
+		}
+
+		if c.toClose {
+			c.conn.Close()
+			return
 		}
 	}
 }

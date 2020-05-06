c5bf968e20a21e2b79a8d35051c73807b660925a
jmichalak9 <michalakjakub1999@gmail.com>
2020-05-06 12:37:45 +0200

    Add some tests

--- pkg/server_test.go
+++ pkg/server_test.go
@@ -4,10 +4,12 @@ import "testing"
 
 type handlerTest struct {
 	title    string
+	c        client
 	argv     string
 	expected string
 }
 
+// Handler tests only check for returned reply code.
 func TestHandleSYST(t *testing.T) {
 	tests := []handlerTest{
 		{
@@ -17,9 +19,17 @@ func TestHandleSYST(t *testing.T) {
 		},
 	}
 	for _, test := range tests {
-		result := handleSYST(test.client, test.argv)
-		if result != test.expected {
-			t.Errorf("failed, got %v, expected %v", result, test.expected)
+		result := handleSYST(&test.c, test.argv)
+		replyCode := replyCodeFromResult(result)
+		if replyCode != test.expected {
+			t.Errorf("failed, got %v, expected %v", replyCode, test.expected)
 		}
 	}
+}
+
+func replyCodeFromResult(result string) string {
+	if len(result) < 3 {
+		return result
+	}
+	return result[0:3]
 }
